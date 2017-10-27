aeff48c4b82e52a0f6d2f2b5c14ea8eff040e94e
Sam Meech-Ward <sam@meech-ward.com>
2017-10-27 09:40:27 -0700

    Create a detail view controller for weather

---
+++ WeatherAppForBadWeather/WeatherDetailsViewController.m
@@ -0,0 +1,22 @@
+//
+//  WeatherDetailsViewController.m
+//  WeatherAppForBadWeather
+//
+//  Created by Sam Meech-Ward on 2017-10-27.
+//  Copyright © 2017 Sam Meech-Ward. All rights reserved.
+//
+
+#import "WeatherDetailsViewController.h"
+
+@interface WeatherDetailsViewController ()
+
+@end
+
+@implementation WeatherDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
